591da4c6ad3c5242758d120b5a770c229183e0b6
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-09-22 22:32:35 +0300

    Backing up.

--- app.js
+++ app.js
@@ -2,15 +2,37 @@
 // Node.js Training Project
 // September 1, 2018
 
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
 const hostname = 'localhost';
 const port = 3000;
+const readStream = {
+  home: fs.createReadStream('./index.html'),
+  search: fs.createReadStream('./search.html'),
+  docs: fs.createReadStream('./docs.html'),
+  noMatch: fs.createReadStream('./noMatch.html')
+  // express.js automatically figure content-type
+};
 
-var express = require('express');
-var app = express();
-
+// define routes
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  readStream.home.pipe(res);
 });
 
+app.get('/search', (req, res) => {
+  readStream.search.pipe(res);
+});
+
+app.get('/docs', (req, res) => {
+  readStream.docs.pipe(res);
+});
+
+app.get('/404', (req, res) => {
+  readStream.noMatch.pipe(res);
+});
+
+// listen to server
 app.listen(port);
 console.log(`Server running at http://${hostname}:${port}`);
