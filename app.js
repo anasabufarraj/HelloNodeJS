6b7908d5887f58fbbac522f0be283cb160097e1a
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-10-05 17:13:18 +0300

    Backing up.

--- app.js
+++ app.js
@@ -3,19 +3,37 @@
 // September 1, 2018
 
 const express = require('express');
-
 const app = express();
+
 const hostname = 'localhost';
 const port = 3000;
 
-app.set('view engine', 'pug');
-app.use('/static', express.static('./static'));
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Hello Pug',
-    name: 'Pooh'
-  });
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/profile/:id', (req, res) => {
+  // TODO: get this from database
+  var userData = {
+    id: req.params.id,
+    firstName: 'John',
+    lastName: 'Peterson',
+    age: 23,
+    active: true,
+    interests: ['Design', 'Web', 'Acting']
+  };
+  res.render('profile', { data: userData });
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(`${__dirname}/login.html`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/404.html`);
 });
 
 app.listen(port);
