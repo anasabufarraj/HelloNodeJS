dee60f538bd2dd69ec7bc78ea8648abaa90415a7
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-10-25 09:41:04 +0300

    Backing up.

--- app.js
+++ app.js
@@ -2,39 +2,10 @@
 // Node.js Training Project
 // September 1, 2018
 
-const express = require('express');
-const app = express();
+const fs = require('fs');
 
-const hostname = 'localhost';
-const port = 3000;
-
-app.set('view engine', 'ejs');
-app.use('/static', express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.get('/profile/:id', (req, res) => {
-  // TODO: get this from database
-  var userData = {
-    id: req.params.id,
-    firstName: 'John',
-    lastName: 'Peterson',
-    age: 23,
-    active: true,
-    interests: ['Design', 'Web', 'Acting']
-  };
-  res.render('profile', { data: userData });
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(`${__dirname}/login.html`);
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/404.html`);
-});
-
-app.listen(port);
-console.log(`Server running at http://${hostname}:${port}`);
+(function readWrite(src, dest, data) {
+  fs.readFileSync(src, 'utf-8');
+  fs.writeFileSync(dest, data);
+  console.log('Successfully written');
+})(`${__dirname}/data/read.txt`, `${__dirname}/data/write.txt`, 'hello');
