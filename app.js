23c0bcdbb5207f16a9c9d04bc4eb2c704554407e
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-11-25 20:50:05 +0200

    Backing up.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 // Node.js Training Project
 // September 1, 2018
 
-// const fs = require('fs');
-// const zlib = require('zlib');
-// const src = `${__dirname}/data/`;
-//
-// // creating read, write, and compress piped streams
-// const readStream = fs.createReadStream(`${src}read.txt`);
-// const writeStream = fs.createWriteStream(`${src}read.txt.gz`);
-// const gzip = zlib.createGzip();
-//
-// readStream.pipe(gzip).pipe(writeStream);
+const fs = require('fs');
+const zlib = require('zlib');
+const src = `${__dirname}/data/`;
+const gzip = zlib.createGzip();
+
+// creating read, write, and compress piped streams
+const readStream = fs.createReadStream(`${src}read.txt`);
+const writeStream = fs.createWriteStream(`${src}read.txt.gz`); // or .gz
+
+readStream.pipe(gzip).pipe(writeStream);
