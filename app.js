18b8bc61f56f62ae90891f236de98c4118422ae0
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-03-18 18:43:38 +0200

    Backing up.

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 // Node.js Training Project
 // September 1, 2018
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('What is your name? ', name => {
-  rl.setPrompt(`Hello ${name}, what year you've been born? `);
-  rl.prompt();
-  rl.on('line', age => {
-    rl.setPrompt(`Hey ${name}, you are ${2018 - age} years old.\n`);
-    rl.prompt();
-    rl.on('close', () => {
-      console.log(`Good Bye...`);
-    });
-    rl.close();
-  });
-});
+// const readline = require('readline');
+//
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+//
+// rl.question('What is your name? ', name => {
+//   rl.setPrompt(`Hello ${name}, what year you've been born? `);
+//   rl.prompt();
+//   rl.on('line', age => {
+//     rl.setPrompt(`Hey ${name}, you are ${2018 - age} years old.\n`);
+//     rl.prompt();
+//     rl.on('close', () => {
+//       console.log(`Good Bye...`);
+//     });
+//     rl.close();
+//   });
+// });
